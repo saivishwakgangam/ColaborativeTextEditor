800d713ecf28177efb3b1f5bac90ea8d664f984e
prudhvi <prudhvi.koppuravuri@students.iiit.ac.in>
2022-04-20 03:55:54 +0530

    added

--- main.js
+++ main.js
@@ -3,10 +3,63 @@ var username;
 var con;
 var editor;
 var dest_user_id;
+var crdt;
+class Identifier {
+    constructor(digit,site){
+        this.digit=digit
+        this.site=site
+    }
+    
+}
+class Char {
+    constructor(Identifiers,value){
+        this.position=Identifiers
+        this.value=value
+    }
+    
+}
+class CRDT{
+    constructor(){
+
+        this.chars=[new Char([new Identifier(0,"0")],"$"),new Char([new Identifier(10000,"0")],"$")]
+        //this.chars=[]
+    }
+    generateIdBetween()
+    generate_pos_btw(pos1,pos2,new_pos=[]){
+        let id1 = pos1[0];
+        let id2 = pos2[0];
+        //console.log(id2,id1)
+        if (id2.digit - id1.digit > 1) {
+
+        let newDigit = this.generateIdBetween(id1.digit, id2.digit);
+        new_pos.push(new Identifier(newDigit, this.siteId));
+        return new_pos;
+
+        } else if (id2.digit - id1.digit === 1) {
+
+        new_pos.push(id1);
+        return this.generate_pos_btw(pos1.slice(1), pos2, new_pos);
+
+        }
+    }
+
+    local_insert(value,index){
+        //console.log(index)
+        const prev_pos=(this.chars[index-1]&&this.chars[index-1].position)||[]
+        const next_pos=(this.chars[index] && this.chars[index].position)||[];
+        console.log(prev_pos,next_pos)
+        const newPos=this.generate_pos_btw(prev_pos,next_pos)
+
+
+
+    }
+}
 $(document).ready(function(){
+    crdt=new CRDT()
     var code=$(".codemirror-textarea")[0]
     editor=CodeMirror.fromTextArea(code,{
-        lineNumbers:true
+        lineNumbers:true,
+        lineWrapping:true
     });
     $("#username_submit_id").on("click",function(){
         test1()
@@ -14,10 +67,14 @@ $(document).ready(function(){
     $("#destid_submit_id").on("click",function(){
         test2()
     })
-    console.log(editor)
     editor.on('keyup',function(cMirror,event){
         data=cMirror.getValue()
+        ch=event.key
+        start_cursor = editor.getCursor();
+        //console.log(start_cursor)
+        crdt.local_insert(ch,start_cursor.line)
         con.send(data)
+        
     })
 
 
